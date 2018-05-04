2e129118b15e3ac3a079ef8eaa4b06db072ff8ec
kevinyk <kevinyoungkim@gmail.com>
2018-05-04 14:32:25 -0700

    solution to original assignment

--- server.py
+++ server.py
@@ -1,34 +1,27 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
-app.secret_key="ActualSecretKey"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/blah', methods=['GET',"POST"])
-def blah():
-    print(request.form)
-    # dump(session)
-    session['name'] = request.form['name']
-    session['location'] = request.form.get('location')
-    session['language'] = request.form.get('language')
-    session['comment'] = request.form.get('comment')
-    return redirect('/result')
-
-@app.route('/result')
+@app.route('/result', methods=["POST"])
 def result():
-    print('hit')
-    name = session['name']
-    location = session['location']
-    language = session['language']
-    comment = session['comment']
+    print(request.form)
+    name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
     return render_template('results.html', name=name, location=location, language=language, comment=comment)
 
 @app.route('/danger')
 def danger():
     print('user tried to enter dangerzone')
     return redirect('/')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
